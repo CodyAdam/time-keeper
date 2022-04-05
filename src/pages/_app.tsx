597438172a6718606232e58b1476a6e13bcc2db2
CodyAdam <cody.ad4@gmail.com>
2022-04-05 17:28:02 +0200

    Added Raleway font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 
 // Place this in the pages/_app.js file
