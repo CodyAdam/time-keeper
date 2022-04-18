23bc1a1e860c08c772099b1d1fb514c3d40790f0
CodyAdam <cody.ad4@gmail.com>
2022-04-18 17:31:44 +0200

    Moved props

--- src/modules/Counter.tsx
+++ src/modules/Counter.tsx
@@ -1,40 +1,7 @@
-import { onSnapshot, runTransaction, DocumentReference, DocumentData } from 'firebase/firestore';
 import { NextPage } from 'next';
-import { useEffect, useState } from 'react';
-import { ButtonGroup, Button } from 'react-bootstrap';
-import { db } from '../pages/api/firebase';
 import styles from '../styles/Counter.module.css';
 
-export async function addCredits(amount: number, userRef: DocumentReference<DocumentData>) {
-  try {
-    await runTransaction(db, async (transaction) => {
-      const docSnap = await transaction.get(userRef);
-      if (!docSnap.exists()) {
-        throw 'Document does not exist!';
-      }
-      const credits = docSnap.data().credits;
-      const newCredits = (credits ? credits : 0) + amount;
-      transaction.update(userRef, { credits: newCredits });
-    });
-  } catch (e) {
-    console.error('Transaction failed: ', e);
-  }
-}
-
-export const Counter: NextPage<{ userRef: DocumentReference<DocumentData> }> = ({ userRef }) => {
-  const [credits, setCredits] = useState(0);
-
-  useEffect(() => {
-    addCredits(0, userRef);
-    const unsub = onSnapshot(userRef, (docSnap) => {
-      if (docSnap.exists()) {
-        setCredits(docSnap.data().credits);
-      } else console.error('Doc not found :', userRef);
-    });
-    return () => {
-      unsub();
-    };
-  }, []);
+export const Counter: NextPage<{ credits: number }> = ({ credits }) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Credits : {credits}</h1>
