574c939f5100f3324187bf017b9ee6a0b62fb0a1
CodyAdam <cody.ad4@gmail.com>
2022-04-18 17:31:37 +0200

    Stylized calendar event

--- src/modules/Calendar.tsx
+++ src/modules/Calendar.tsx
@@ -1,15 +1,52 @@
 import styles from '../styles/Default.module.css';
 import { NextPage } from 'next';
 import moment from 'moment';
-import { Calendar as Cal, momentLocalizer } from 'react-big-calendar';
+import { Calendar as BigCalendar, momentLocalizer, Views } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { useEffect, useMemo } from 'react';
 
-export const Calendar: NextPage = () => {
+export type Event = {
+  title: string;
+  start: Date;
+  end: Date;
+  multiplier: number;
+  cost: number;
+  hexColor?: string;
+};
+
+function eventStyleGetter(event: Event, start: Date, end: Date, isSelected: boolean) {
+  var backgroundColor = event.hexColor ? '#' + event.hexColor : '#00a1ff';
+  var style = {
+    backgroundColor: backgroundColor,
+    opacity: 0.9,
+    color: 'white',
+    border: '0px',
+  };
+  return {
+    style: style,
+  };
+}
+
+export const Calendar: NextPage<{ events: Event[] }> = ({ events }) => {
   const localizer = momentLocalizer(moment); // or globalizeLocalizer
+  const views = useMemo(() => ({ week: true, day: true }), []);
+
+  useEffect(() => {
+    console.log('events', events);
+
+    return () => {};
+  }, [events]);
 
   return (
     <div className={styles.container}>
-      <Cal localizer={localizer} startAccessor='start' endAccessor='end' />
+      <BigCalendar
+        localizer={localizer}
+        defaultView={Views.DAY}
+        defaultDate={new Date()}
+        views={views}
+        events={events}
+        eventPropGetter={eventStyleGetter}
+      />
     </div>
   );
 };
