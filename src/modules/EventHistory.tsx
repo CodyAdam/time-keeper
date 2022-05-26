d69ad96a6b4429e2c84f400d99b0d302f05df8da
CodyAdam <cody.ad4@gmail.com>
2022-05-26 22:53:31 +0200

    feat: add more info on history card

--- src/modules/EventHistory.tsx
+++ src/modules/EventHistory.tsx
@@ -6,14 +6,19 @@ import moment from 'moment';
 
 export const EventHistory: NextPage<{ events: Event[] }> = ({ events }) => {
   const cards = events.map((e: Event) => {
-    const date: string = moment(e.start).format('MMMM Do YYYY');
+    const date: string = moment(e.start).format('MMMM Do');
+    const time : string = moment(e.start).format('H:mm');
     return (
       <Card style={{ margin: '1rem' }} key={e.start.toISOString()}>
         <Card.Body>
           <Card.Title>{e.title}</Card.Title>
           <div className={styles.leftright}>
-            {e.cost == 0? 'Free' : `${e.cost} credits`}
+            {e.cost == 0 ? 'Free' : `${e.cost} credits`}
             <div>{date}</div>
+          </div>
+          <div className={styles.leftright}>
+            Multiplier : x{e.multiplier}
+            <div>{time}</div>
           </div>
         </Card.Body>
       </Card>
